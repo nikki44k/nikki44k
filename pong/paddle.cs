07fd586210c48b993df6c20839d75db88234f8b8
nikki44k <154763746+nikki44k@users.noreply.github.com>
2024-01-16 12:46:56 -0800

    Create paddle.cs

---
+++ pong/paddle.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddle: 
+  MonoBehaviour{
+
+    void start(){
+      
+    }
+    public void Init(bool is_rpaddle){
+      if (is_rpaddle){      //paddle will be on the rightside
+        
+      }else{                //paddle will be on the left side
+        
+      }
+    }
+
+    void update(){
+      
+    }
+    
+  }
