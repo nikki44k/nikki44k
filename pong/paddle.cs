bf377d86f9501d0ad470d6a1352770c55bb316af
nikki44k <154763746+nikki44k@users.noreply.github.com>
2024-01-16 13:14:36 -0800

    Update paddle.cs

--- pong/paddle.cs
+++ pong/paddle.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class paddle: 
   MonoBehaviour{
-
+    float height;
+    float speed;
+    bool is_r;
+    string input;
     void start(){
-      
+      speed = 5f;
+      height = transform.localScale.y;      
     }
     public void Init(bool is_rpaddle){
+      is_r = is_rpaddle;
+      v pos = v.zero;
       if (is_rpaddle){      //paddle will be on the rightside
-        
+        pos = new v(manager.topRight.x, 0);   
+        pos -= v.right * transform.localScale.x;  //moves to left
       }else{                //paddle will be on the left side
-        
+        pos = new v(manager.bottomLeft.x, 0);
+        pos += v.right * transform.localScale.x;  //moves to right
       }
+      transform.position = pos;
     }
 
     void update(){
