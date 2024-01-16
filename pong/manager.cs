0016caf90e07eb16450cbabc4d828237856fa60b
nikki44k <154763746+nikki44k@users.noreply.github.com>
2024-01-16 12:51:52 -0800

    Update manager.cs

--- pong/manager.cs
+++ pong/manager.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class manager: 
-  behaviour{
+  MonoBehaviour{
     public ball b;
     public paddle p;
+
+    public static v topRight;
+    public static v bottomLeft;
   }
 
 void start(){
@@ -17,7 +20,11 @@ void start(){
   paddle p2 = Instantiate (p) as paddle;
   p1.Init(true);  //paddle one
   p2.Init(false);  //paddle two
+
+  bottomLeft = Camera.main.ScreenToWorldPoint(new v(0, 0));    //converting screen pixel into game coordinates
+  topRight = Camera.main.ScreenToWorldPoint(new v(Screen.width, Screen.height));
 }
+
 void update(){
   
 }
