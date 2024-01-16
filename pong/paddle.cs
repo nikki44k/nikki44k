a01d5e0349cc76b97398a9854dc62d7763de24a5
nikki44k <154763746+nikki44k@users.noreply.github.com>
2024-01-16 13:19:25 -0800

    Update paddle.cs

--- pong/paddle.cs
+++ pong/paddle.cs
@@ -12,21 +12,27 @@ public class paddle:
       speed = 5f;
       height = transform.localScale.y;      
     }
+    
     public void Init(bool is_rpaddle){
       is_r = is_rpaddle;
       v pos = v.zero;
       if (is_rpaddle){      //paddle will be on the rightside
         pos = new v(manager.topRight.x, 0);   
         pos -= v.right * transform.localScale.x;  //moves to left
+        input = "PaddleRight";
+        
       }else{                //paddle will be on the left side
         pos = new v(manager.bottomLeft.x, 0);
         pos += v.right * transform.localScale.x;  //moves to right
+        input = "PaddleLeft";
       }
       transform.position = pos;
+      transform.name = input;
     }
 
     void update(){
-      
+      float move = Input.GetAxis(input) * Time.deltaTime * speed;
+      transform.Translate(move * v.up);
     }
     
   }
