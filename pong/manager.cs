4e08f827c3c0566a34c444b5c3f9b55494507b14
nikki44k <154763746+nikki44k@users.noreply.github.com>
2024-01-17 00:58:06 -0800

    Update manager.cs

--- pong/manager.cs
+++ pong/manager.cs
@@ -2,29 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class manager: 
-  MonoBehaviour{
+public class NewBehaviourScript : MonoBehaviour{
     public ball b;
     public paddle p;
 
-    public static v topRight;
-    public static v bottomLeft;
-  }
+    public static Vector2 bottomLeft;
+    public static Vector2 topRight;
 
-void Start(){
-  //Ball created
-  Instantiate (b);
-  
-  //Two paddles created on both sides
-  paddle p1 = Instantiate (p) as paddle;
-  paddle p2 = Instantiate (p) as paddle;
-  p1.Init(true);  //paddle one
-  p2.Init(false);  //paddle two
+    // Start is called before the first frame update
+    void Start(){
 
-  bottomLeft = Camera.main.ScreenToWorldPoint(new v(0, 0));    //converting screen pixel into game coordinates
-  topRight = Camera.main.ScreenToWorldPoint(new v(Screen.width, Screen.height));
-}
+        bottomLeft = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));    //converting screen pixel into game coordinates
+        topRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
 
-void Update(){
-  
+        //Ball created
+        Instantiate(b);
+
+        //Two paddles created on both sides
+        paddle p1 = Instantiate(p) as paddle;
+        paddle p2 = Instantiate(p) as paddle;
+        p1.Init(true);  //paddle one
+        p2.Init(false);  //paddle two
+
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+    }
 }
