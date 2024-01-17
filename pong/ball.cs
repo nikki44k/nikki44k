16dcc186fe639d0929c2e30923fe212a8a5aeb1d
nikki44k <154763746+nikki44k@users.noreply.github.com>
2024-01-17 00:58:53 -0800

    Update ball.cs

--- pong/ball.cs
+++ pong/ball.cs
@@ -2,51 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ball:
-  MonoBehaviour{
+public class ball : MonoBehaviour{
     
     [SerializeField]
     float speed;
-    
+
     float radius;
-    v direction;  
-    
-  void Start(){
-    direction = v.one.normalized;
-    radius = transform.localScale.x / 2;
-  }
+    Vector2 direction;
 
-  void Update(){
-    transform.Translate(direction * speed * Time.delatatime);
-    if(transform.position.y < manager.bottomLeft.y + radius && direction.y < 0){
-        direction.y = - direction.y;
-    }
-    if(transform.position.y > manager.topright.y - radius && direction.y > 0){
-        direction.y = - direction.y;
+    void Start()
+    {
+        direction = Vector2.one.normalized;
+        radius = transform.localScale.x / 2;
     }
 
-    //Game ends
-    if(transform.position.x < manager.bottomLeft.x + radius && direction.x < 0){
-      Debug.Log("Right player wins!!");
-      Time.timeScale = 0;
-      enabled = false;
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+        if (transform.position.y < NewBehaviourScript.bottomLeft.y + radius && direction.y < 0)
+        {
+            direction.y = -direction.y;
+        }
+        if (transform.position.y > NewBehaviourScript.topRight.y - radius && direction.y > 0)
+        {
+            direction.y = -direction.y;
+        }
+
+        //Game ends
+        if (transform.position.x < NewBehaviourScript.bottomLeft.x + radius && direction.x < 0)
+        {
+            Debug.Log("Right player wins!!");
+            Time.timeScale = 0;
+            enabled = false;
+        }
+        if (transform.position.x > NewBehaviourScript.topRight.x - radius && direction.x > 0)
+        {
+            Debug.Log("Left player wins!!");
+            Time.timeScale = 0;
+            enabled = false;
+        }
     }
-    if(transform.position.x > manager.topRight.x - radius && direction.x > 0){
-      Debug.Log("Left player wins!!");
-      Time.timeScale = 0;
-      enabled = false;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "paddle"){
+            bool is_r = other.GetComponent<paddle>().is_r;
+            if (is_r == true && direction.x > 0)
+            {
+                direction.x = -direction.x;
+            }
+            if (is_r == false && direction.x < 0)
+            {
+                direction.x = -direction.x;
+            }
+        }
     }
-  }
-  void OnTriggerEnter2D(Collider2D other){
-    if(other.tag == "paddle"){
-      bool is_r = other.GetComponent<paddle>().is_r;
-      if(is_r == true && direction.x > 0){
-        direction.x = - direction.x;
-      }
-      if(is_r == false && direction.x < 0){
-        direction.x = - direction.x;
-      }
-    }
-  }
 }
-  
